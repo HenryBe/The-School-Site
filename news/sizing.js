cec4a2f18d7faab11923b567b3a25c9957c955e7
HenryBe <0x004eb480@gmail.com>
2016-12-02 18:52:35 +0800

    News page updated,bootstrap and jQuery added

--- news/sizing.js
+++ news/sizing.js
@@ -1,9 +1,13 @@
-function enlarge(id)
-{
-	document.getElementById(id).className="col-lg-4 col-md-6 col-sm-8" ;
-	this.innerHTML="fdfdsfsdfds";
-}
 function resize(id)
 {
-	document.getElementById(id).className="col-lg-2 col-md-3 col-sm-4";
+}
+function enlarge(id)
+{
+	var list=document.getElementsByClassName("col-lg-4");
+	for(var x=0;x<list.length;x++)
+	{
+		list[x].className='col-lg-2 col-md-3 col-sm-4';
+	}
+	document.getElementById(id).className="col-lg-4 col-md-6 col-sm-8" ;
+	
 }
